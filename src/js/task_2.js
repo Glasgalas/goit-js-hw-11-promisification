4b9c4b0eb8b0dff35e8cd4ebd237a4423a42147f
Glasgalas <alex.starichenko@icloud.com>
2021-02-11 22:50:49 +0200

    refactoring

--- src/js/task_2.js
+++ src/js/task_2.js
@@ -17,24 +17,9 @@ const toggleUserState = (allUsers, userName) => {
   });
 };
 
-// const toggleUserState = (allUsers, userName, callback) => {
-//   const updatedUsers = allUsers.map((user) =>
-//     user.name === userName ? { ...user, active: !user.active } : user
-//   );
-
-//   callback(updatedUsers);
-// };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, "Mango", logger);
-// toggleUserState(users, "Lux", logger);
 
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
